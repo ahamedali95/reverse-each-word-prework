b9ae51a2f46238362306b07666c17373ce1b17e6
AHAMED ABBAS <ahamedali95@gmail.com>
2018-03-17 18:02:51 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,19 @@
+# def reverse_each_word(sentence)
+#   words = sentence.split(" ")
+# 
+#   words.each_with_index do |word, idx|
+#     words.slice!(idx, 1)
+#     words.insert(idx, word.reverse())
+#   end
+# 
+#   words.join(" ")
+# end
+
 def reverse_each_word(sentence)
   words = sentence.split(" ")
 
-  words.each_with_index do |word, idx|
-    words.slice!(idx, 1)
-    words.insert(idx, word.reverse())
+  words.collect do |word|
+    word.reverse()
   end
-
-  words.join(" ")
 end
+
